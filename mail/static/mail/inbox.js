ee2858ef175f4b9fb1f77264e54c81a8928b62f2
joselws <josel.wilhelms@gmail.com>
2021-07-22 18:50:35 -0400

    Send Email and Mailbox view working

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,10 +5,33 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
+  
   // By default, load the inbox
   load_mailbox('inbox');
+  
+  // Send email logic
+  document.querySelector('#compose-form').addEventListener('submit', async () => {
+      // Submit an email
+      await fetch('/emails', {
+        method: 'POST',
+        body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+        })
+      })
+      .then(response => response.json())
+      .then(result => {
+          // Print result
+          console.log(result.message);
+          load_mailbox('sent');
+      }).catch(error => {
+        console.log(error.message);
+      });
+  
+  });
 });
+
 
 function compose_email() {
 
@@ -16,7 +39,7 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
-  // Clear out composition fields
+  // Input fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
@@ -30,4 +53,33 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Load mails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      emails.forEach(email => {
+        const box = document.createElement('div');
+        box.setAttribute('class', 'email');
+        if (email.read) {
+          box.style.backgroundColor = 'lightgray';
+        }
+        else {
+          box.style.backgroundColor = 'white';
+        }
+
+        let emailInfo = '';
+        if (mailbox === 'inbox') {
+          emailInfo = email.sender;
+        }
+        else {
+          emailInfo = email.recipients;
+        }
+
+        box.innerHTML = `<strong>${emailInfo}:</strong> ${email.subject} <span class="date">${email.timestamp}</span>`;
+
+        // show each email on its respective mailbox view
+        document.querySelector('#emails-view').append(box);
+      });
+    });
 }
